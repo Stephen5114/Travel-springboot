b6ed0b7d4dbc126e75470fdc155c9b84abc30014
李子扬 <513523798@qq.com>
2025-05-04 01:11:21 +0800

    update01
    update01

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,18 +4,15 @@ import Home from '../views/Home.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {path: '/', redirect: '/manager/home'},
     {
-      path: '/',
+      path: '/manager',
       component: import('../views/Manager.vue'),
-      redirect: '/home',
       children: [
-        {path: 'home', component: () => import('../views/Home.vue')}
+        {path: 'home', component: () => import('../views/Home.vue')},
+        {path: 'about', component: import('../views/about.vue')}
       ]
     },
-    /*{
-      path: 'home',
-      component: import('../views/Home.vue')
-    }, **/
     {
       path: '/about',
       component: import('../views/about.vue')
