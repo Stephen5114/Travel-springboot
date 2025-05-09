d21a8d07f9822043b19432853f6eca38e76a8365
李子扬 <513523798@qq.com>
2025-05-09 21:40:36 +0800

    4
    haha

--- springboot/src/main/java/com/example/service/AdminService.java
+++ springboot/src/main/java/com/example/service/AdminService.java
@@ -1,15 +1,27 @@
 package com.example.service;
 
+import com.example.entity.Admin;
 import com.example.exception.CustomException;
+import com.example.mapper.AdminMapper;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdminService {
+    @Resource
+    AdminMapper adminMapper;
+
     public String admin(String name){
         if(name.equals("admin")){
             return "admin";
         }else {
             throw new CustomException("!!!!!CUSTOM ERROR!!!!");
         }
+    }
+
+    public List<Admin> selectAll(){
+        return adminMapper.selectAll();
     }
 }
